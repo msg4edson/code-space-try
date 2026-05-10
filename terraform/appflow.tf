36cebe62904c3b722e424cdabb541dd2999a0bb5
msg4edson <32358101+msg4edson@users.noreply.github.com>
2026-05-10 05:14:27 +0000

    feat: Add IAM policy for AppFlow access to S3 and update dependencies in AppFlow configuration

--- terraform/appflow.tf
+++ terraform/appflow.tf
@@ -4,6 +4,14 @@ resource "aws_appflow_flow" "servicenow_to_s3" {
   name    = var.appflow_name
   kms_arn = var.appflow_kms_arn
 
+  depends_on = [
+    aws_s3_bucket_policy.servicenow_ingestion,
+    aws_s3_bucket_public_access_block.servicenow_ingestion,
+    aws_s3_bucket_server_side_encryption_configuration.servicenow_ingestion,
+    aws_s3_bucket_versioning.servicenow_ingestion,
+    aws_appflow_connector_profile.servicenow
+  ]
+
   source_flow_config {
     connector_type         = "Servicenow"
     connector_profile_name = var.create_connector_profile ? aws_appflow_connector_profile.servicenow[0].name : var.servicenow_connector_profile_name
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,6 +2,33 @@ resource "aws_s3_bucket" "servicenow_ingestion" {
   bucket = var.s3_bucket_name
 }
 
+data "aws_iam_policy_document" "appflow_s3_access" {
+  statement {
+    sid = "AllowAppFlowS3Access"
+
+    principals {
+      type        = "Service"
+      identifiers = ["appflow.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+
+    resources = [
+      aws_s3_bucket.servicenow_ingestion.arn,
+      "${aws_s3_bucket.servicenow_ingestion.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "servicenow_ingestion" {
+  bucket = aws_s3_bucket.servicenow_ingestion.id
+  policy = data.aws_iam_policy_document.appflow_s3_access.json
+}
+
 resource "aws_s3_bucket_versioning" "servicenow_ingestion" {
   count  = var.manage_s3_bucket_security_resources ? 1 : 0
   bucket = aws_s3_bucket.servicenow_ingestion.id
